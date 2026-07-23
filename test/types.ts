f3697cabba37d79935c91e96784c8bace8247af5
Andrey Sitnik <andrey@sitnik.es>
2026-07-23 11:03:49 +0000

    Fix import

--- test/types.ts
+++ test/types.ts
@@ -2,7 +2,6 @@ import { eq } from 'drizzle-orm'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { drizzle } from 'drizzle-orm/sqlite-proxy'
 
-import type { Driver, DriverTransaction, SqlParam } from '../index.js'
 import { openDb, toDrizzle } from '../index.js'
 import { nodeDriver } from '../node/index.js'
 
